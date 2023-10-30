712fdf65edf6b7b93789bede79d4a0b61975cc49
William <bouw1301@usherbrooke.ca>
2023-10-30 19:47:57 -0400

    Debut du RN, 85% acc

--- helpers/ImageCollection.py
+++ helpers/ImageCollection.py
@@ -81,11 +81,11 @@ class ImageCollection:
 
             #rgb_variance = self.getRGBVariance(imageRGB)/10
             lightness_max = self.max_luminence(imageRGB)/10
-            #rgb_stand_er = self.standard_error_RGB(imageRGB)*3000
+            rgb_stand_er = self.standard_error_RGB(imageRGB)*3000
             xyz_stand_er = self.standard_error_XYZ(imageRGB)*1000000
-            #noise = self.calculate_noise(imageRGB)*10000
+            noise = self.calculate_noise(imageRGB)*10000
 
-            data = np.array([lightness_max, xyz_stand_er])
+            data = np.array([xyz_stand_er, noise, lightness_max, rgb_stand_er])
             img_class = self.labels[image_counter]
             if img_class == 1:
                 coast.append(data)
@@ -99,9 +99,9 @@ class ImageCollection:
         npforest = np.array(forest)
         npstreet = np.array(street)
 
-        dataList.append(npcoast[:200])
-        dataList.append(npforest[:200])
-        dataList.append(npstreet[:200])
+        dataList.append(npcoast[:250])
+        dataList.append(npforest[:250])
+        dataList.append(npstreet[:250])
 
 
         return dataList
--- problematique.py
+++ problematique.py
@@ -5,8 +5,12 @@ Problématique APP2 Module IA S8
 
 import matplotlib.pyplot as plt
 
+from keras.optimizers import Adam
+import keras as K
+
 from helpers.ClassificationData import ClassificationData
 from helpers.ImageCollection import ImageCollection
+import helpers.classifiers as classifiers
 
 
 #######################################
@@ -20,8 +24,19 @@ def problematique_APP2():
         points = images.prep_images()
         data = ClassificationData(points)
         data.getStats(gen_print=True)
-        # Figure avec les ellipses et les frontières
-        data.getBorders(view=True)
+
+        n_neurons = 7
+        n_layers = 6
+
+        nn = classifiers.NNClassify_APP2(data2train=data, data2test=data,
+                                          n_layers=n_layers, n_neurons=n_neurons, innerActivation='relu',
+                                          outputActivation='softmax', optimizer=Adam(), loss='categorical_crossentropy',
+                                          metrics=['accuracy'],
+                                          callback_list=[K.callbacks.EarlyStopping(patience=50, verbose=1, restore_best_weights=1),
+                                                         classifiers.print_every_N_epochs(25)],     # TODO à compléter L2.E4
+                                          experiment_title='NN Simple',
+                                          n_epochs = 1000, savename='classification',
+                                          ndonnees_random=5000, gen_output=True, view=True)
         #N = 6
         #im_list = images.get_samples(N)
         #print(im_list)
